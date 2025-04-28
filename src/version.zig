4c5a0c0df6becec843d21b065e11242a75a3ad4f
renerocksai <rene@renerocks.ai>
2025-04-28 10:49:04 +0200

    fi_home in dashboard, reformat version.zig

--- src/version.zig
+++ src/version.zig
@@ -1,17 +1,29 @@
 const std = @import("std");
 const Format = @import("format.zig");
-const zon = @import("build.zig.zon");
+
+const build_zig_zon = @import("build.zig.zon");
 
 pub fn print_version() void {
-    std.debug.print("version is `{s}`", .{version() orelse "(unknown version)"});
+    std.debug.print(
+        "version is `{s}`",
+        .{version() orelse "(unknown version)"},
+    );
 }
 
 pub fn version() ?[]const u8 {
-    var it = std.mem.splitScalar(u8, zon.contents, '\n');
+    var it = std.mem.splitScalar(
+        u8,
+        build_zig_zon.contents,
+        '\n',
+    );
     while (it.next()) |line_untrimmed| {
         const line = Format.strip(line_untrimmed);
         if (std.mem.startsWith(u8, line, ".version")) {
-            var tokenizer = std.mem.tokenizeAny(u8, line[".version".len..], " \"=");
+            var tokenizer = std.mem.tokenizeAny(
+                u8,
+                line[".version".len..],
+                " \"=",
+            );
             return tokenizer.next();
         }
     }
