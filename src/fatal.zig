11e66c57b9f08da39b92dad753dc15de52fd84dc
renerocksai <rene@renerocks.ai>
2025-07-08 09:30:55 +0200

    reworked workdir handling everywhere, no chdir anymore

--- src/fatal.zig
+++ src/fatal.zig
@@ -15,10 +15,12 @@ pub fn fatal(comptime fmt: []const u8, args: anytype, err: anyerror) !noreturn {
                         // as much as possible WAS written to the buffer
                         // so the entire buffer must be full
                         errormsg = errormsg_buffer[0..];
+                        std.log.err("{s}", .{errormsg});
                         return err;
                     },
                 }
             };
+            std.log.err("{s}", .{errormsg});
             return err; // so that server can catch it
         },
     }
